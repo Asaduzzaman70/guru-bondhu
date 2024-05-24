41410174a806c9e7b2656ba897249ed0057bf4e6
Asaduzzaman70 <asdudjamanhisam@gmail.com>
2024-05-24 09:16:47 +0600

    Create router, contexts and some file created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
+import DarkModeToggle from './DarkModeToggle'
 function App() {
 
   return (
     <>
-      <div className="bg-white text-black dark:bg-gray-900 dark:text-white min-h-screen flex items-center justify-center">
+      <div className="bg-myColor-light text-black dark:bg-gray-900 dark:text-white min-h-screen flex items-center justify-center">
         <div className="p-4">
-          <p className="text-gray-700 dark:text-3xl dark:text-gray-300">
-            This text will change color based on the theme.
+          <DarkModeToggle/>
+          <p className="text-gray-700 dark:text-gray-300">
+            <span className='dark:hidden'>This text will change color based on the theme.</span>
+            <span className='hidden dark:inline-block'>This text will change color based on the theme.......................</span>
           </p>
         </div>
       </div>
--- src/contexts/AuthProvider.jsx
+++ src/contexts/AuthProvider.jsx
@@ -1,7 +1,20 @@
-const AuthProvider = () => {
+import { createContext, useState } from "react";
+
+export const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+
+    const authInfo = {
+        user,
+        loading
+    }
     return (
-        <>
-        </>
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
     );
 };
 
